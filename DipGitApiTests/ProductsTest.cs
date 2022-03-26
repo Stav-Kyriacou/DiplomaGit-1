f6915798b1caf8e618ade3f07da37db81d771921
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-26 12:10:50 +1100

    TotalValue test

--- DipGitApiTests/ProductsTest.cs
+++ DipGitApiTests/ProductsTest.cs
@@ -22,5 +22,20 @@ namespace DipGitApiTests
 
             Assert.Equal(expected, products.GetTotalQtyProducts());
         }
+        [Theory]
+        [InlineData(0.5f, 1.5f, 2, 4)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(0.5f, 0.5f, 0.5f, 1.5f)]
+        [InlineData(-1.5f, -2.5f, 3.5f, -0.5f)]
+        public void TotalValueTheory(float price1, float price2, float price3, float expected)
+        {
+            var products = new Products();
+            products.ProductList = new List<Product>();
+            products.ProductList.Add(new Product { Name = "Test", Price = price1, Qty = 1 });
+            products.ProductList.Add(new Product { Name = "Test", Price = price2, Qty = 1 });
+            products.ProductList.Add(new Product { Name = "Test", Price = price3, Qty = 1 });
+
+            Assert.Equal(expected, products.GetTotalValueProducts());
+        }
     }
 }
