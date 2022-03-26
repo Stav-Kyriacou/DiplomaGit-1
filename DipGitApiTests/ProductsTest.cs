1edb344ed4f335ba2a9b2dfc666f6b86a365517f
Stavros-Kyriacou <60002156+Stavros-Kyriacou@users.noreply.github.com>
2022-03-26 11:16:11 +1100

    TotalQty test

--- DipGitApiTests/ProductsTest.cs
+++ DipGitApiTests/ProductsTest.cs
@@ -1,14 +1,26 @@
 using System;
+using System.Collections.Generic;
+using DipGitApiLib;
 using Xunit;
 
 namespace DipGitApiTests
 {
     public class ProductsTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(-1, -2, -3, -6)]
+        [InlineData(-1, -2, 3, 0)]
+        public void TotalQtyTheory(int qty1, int qty2, int qty3, int expected)
         {
+            var products = new Products();
+            products.ProductList = new List<Product>();
+            products.ProductList.Add(new Product { Name = "Test", Price = 1.5f, Qty = qty1 });
+            products.ProductList.Add(new Product { Name = "Test", Price = 1.5f, Qty = qty2 });
+            products.ProductList.Add(new Product { Name = "Test", Price = 1.5f, Qty = qty3 });
 
+            Assert.Equal(expected, products.GetTotalQtyProducts());
         }
     }
 }
